a8eabaacd6563c883df384e963dc81a2b4574bec
giash459 <mdgiash999ctg@gmail.com>
2025-11-19 20:03:36 +0600

    added navbar section

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import RootLayout from './layouts/RootLayout.jsx';
+import Home from './components/Home/Home.jsx';
+import AvailableFoods from './components/AvailableFoods/AvailableFoods.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: Home
+      },
+      {
+        path: 'availableFoods',
+        Component: AvailableFoods
+      }
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
